661da1aa2c06006a117709db61fb807b40ff1e64
domlondon <40406207+domlondon@users.noreply.github.com>
2018-06-19 13:06:42 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,12 +3,12 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log('I am ready!');
-  });
+  ]);
   
   client.on('message", nessage => {
     if (message.content === 'ping') {
     message.reply('pong');
         }
-   });
+   ]);
    // THIS MUST BE THIS WAY
    client.login(process.env.BOT_TOKEN);
